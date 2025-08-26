6e31a6394756217f7f8dae45155341a8884f1082
rashid.gta <rashid.gta@gmail.com>
2025-08-26 01:45:22 -0400

    changes to KafkaEnduserApplication & KafkaConfig

--- src/main/java/com/springboot/restapi/kafka/enduser/config/KafkaConfig.java
+++ src/main/java/com/springboot/restapi/kafka/enduser/config/KafkaConfig.java
@@ -9,7 +9,6 @@ public class KafkaConfig {
 //    @kafkaListener annotation will set this method to listen to mentioned topic
     @KafkaListener(topics = AppConstant.LOCATION_TOPIC_NAME, groupId = AppConstant.GROUP_ID)
     public void updatedLocation(String value) {
-
         System.out.println(value);
     }
 }
